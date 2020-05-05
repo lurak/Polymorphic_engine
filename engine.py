8c7f4be46b333cc759ced9f0c65a07830b15556d
Titov Ihor <44226375+lurak@users.noreply.github.com>
2020-05-05 21:11:44 +0300

    modification

--- engine.py
+++ engine.py
@@ -15,8 +15,9 @@ class SimplePol:
         self.counter = 0
         self.content = list()
         self.mov_lst = list()
-        self.sub_lst = list()
-        self.add_lst = list()
+        self.add_sub_lst = list()
+        self.add_sub_lst_im = list()
+        self.add_sub_lst_reg = list()
 
     def reader(self):
         """
@@ -45,19 +46,24 @@ class SimplePol:
         """
         self.parser("mov", self.mov_lst)
 
-    def parser_sub(self):
+    def parser_add_sub(self):
         """
-        Find all sub commands in asm file.
+        Find all add and sub commands in asm file.
         :return: None
         """
-        self.parser("sub", self.sub_lst)
+        self.parser(r"add|sub", self.add_sub_lst)
 
-    def parser_add(self):
+    def classification_add_sub(self):
         """
-        Find all add commands in asm file.
+        Function to divide to different lists add and sub command. Add and sub with
+        immediate and add with register.
         :return: None
         """
-        self.parser("add", self.add_lst)
+        for i in range(len(self.add_sub_lst)):
+            if re.search(r', ?[0-9]*', self.add_sub_lst[i][0]):
+                self.add_sub_lst_im.append(self.add_sub_lst[i])
+            else:
+                self.add_sub_lst_reg.append(self.add_sub_lst[i])
 
     @staticmethod
     def number_division(number):
@@ -81,94 +87,101 @@ class SimplePol:
         i = 0
         while line[i] != ',':
             new_line += line[i]
-            i+=1
+            i += 1
         new_line += ','
         new_line += str(number)
         return new_line
 
-    def nope_adder(self, lst):
+    def nope_adder(self, element):
         """
         Add to asm code nop.
-        :param lst: list of strings
+        :param element: tuple of elements
         :return: None
         """
-        for i in range(len(lst)):
-            index = lst[i][1]
-            self.content.insert(index + self.counter, ['nop'])
-            self.counter += 1
+        index = element[1]
+        self.content.insert(index + self.counter, ['nop'])
+        self.counter += 1
 
-    def division_adder(self, lst):
+    def division_adder_im(self, element):
         """
         Add to asm code divided command of add or sub like:
         add eax 5
         add eax 3
         Instead of add eax 8
-        :param lst: list of strings
+        :param element: tuple of elements
         :return: None
         """
-        for i in range(len(lst)):
-            length = len(lst[i][0])
-            number = str()
-            while lst[i][0][length - 1] != ',':
-                number += lst[i][0][length - 1]
-                length -= 1
-            number = int(number[::-1])
-            div = self.number_division(number)
-            new_line = self.line_maker(lst[i][0], div)
-            self.content.insert(lst[i][1] + self.counter, [new_line])
-            self.counter += 1
-            self.content[self.content.index([lst[i][0]])] = \
-                [self.line_maker(lst[i][0], number - div)]
+        length = len(element[0])
+        number = str()
+        while element[0][length - 1] != ',':
+            number += element[0][length - 1]
+            length -= 1
+        number = int(number[::-1])
+        div = self.number_division(number)
+        new_line = self.line_maker(element[0], div)
+        self.content.insert(element[1] + self.counter, [new_line])
+        self.counter += 1
+        self.content[self.content.index([element[0]])] = \
+            [self.line_maker(element[0], number - div)]
 
-    def stack_adder(self, lst):
+    def add_sub_adder(self, element):
+        """
+        Function which add two lines with add some number to eax and sub
+        this number from eax register.
+        :param element: tuple of elements
+        :return: None
+        """
+        index = element[1]
+        self.content.insert(index + self.counter, ['add eax, 8'])
+        self.counter += 1
+        self.content.insert(index + self.content, ['sub eax, 8'])
+        self.counter += 1
+
+    def stack_adder(self, element):
         """
         Add to asm code push and pop of some register.
-        :param lst: list of strings
+        :param element: tuple of elements
         :return: None
         """
-        for i in range(len(lst)):
-            index = lst[i][1]
-            self.content.insert(index + self.counter, ['push eax'])
-            self.counter += 1
-            self.content.insert(index + self.counter, ['pop eax'])
-            self.counter += 1
+        index = element[1]
+        self.content.insert(index + self.counter, ['push eax'])
+        self.counter += 1
+        self.content.insert(index + self.counter, ['pop eax'])
+        self.counter += 1
 
     def mov_transformer(self):
         """
         Modify every mov command.
         :return: None
         """
-        choice = random.choice([1, 2])
-        if choice == 1:
-            self.nope_adder(self.mov_lst)
-        else:
-            self.stack_adder(self.mov_lst)
+        for i in range(len(self.mov_lst)):
+            choice = random.choice([1, 2])
+            if choice == 1:
+                self.nope_adder(self.mov_lst[i])
+            else:
+                self.stack_adder(self.mov_lst[i])
 
-    def sub_transformer(self):
+    def add_sub_transformer(self):
         """
-        Modify every sub command.
+        Modify every add and sub command.
         :return: None
         """
-        choice = random.choice([1, 2, 3])
-        if choice == 1:
-            self.nope_adder(self.sub_lst)
-        elif choice == 2:
-            self.stack_adder(self.sub_lst)
-        else:
-            self.division_adder(self.sub_lst)
-
-    def add_transformer(self):
-        """
-        Modify every add command.
-        :return: None
-        """
-        choice = random.choice([1, 2, 3])
-        if choice == 1:
-            self.nope_adder(self.add_lst)
-        elif choice == 2:
-            self.stack_adder(self.add_lst)
-        else:
-            self.division_adder(self.add_lst)
+        for i in range(len(self.add_sub_lst_im)):
+            choice = random.choice([1, 2, 3])
+            if choice == 1:
+                self.nope_adder(self.add_sub_lst_im[i])
+            elif choice == 2:
+                self.stack_adder(self.add_sub_lst_im[i])
+            else:
+                self.division_adder_im(self.add_sub_lst_im[i])
+        for i in range(len(self.add_sub_lst_reg)):
+            choice = random.choice([1, 2, 3])
+            if choice == 1:
+                self.nope_adder(self.add_sub_lst_reg[i])
+            elif choice == 2:
+                self.stack_adder(self.add_sub_lst_reg[i])
+            else:
+                self.add_sub_adder(self.add_sub_lst_reg[i])
 
     def polymorph(self):
         """
@@ -176,12 +189,11 @@ class SimplePol:
         :return:  None
         """
         self.reader()
-        self.parser_sub()
+        self.parser_add_sub()
         self.parser_mov()
-        self.parser_add()
+        self.classification_add_sub()
         self.mov_transformer()
-        self.sub_transformer()
-        self.add_transformer()
+        self.add_sub_transformer()
         content = str()
         for i in range(len(self.content)):
             content += self.content[i][0]
@@ -192,5 +204,5 @@ class SimplePol:
 
 
 if __name__ == "__main__":
-    a = SimplePol("asm/bubble_sort.asm")
+    a = SimplePol("simple.asm")
     a.polymorph()
