a14c713cc677d4dfb5675f44bfda18c72b91adaa
Sluzhynskyi <sluzhynskyi@gmail.com>
2020-06-11 22:50:08 +0300

    xoring nops

--- elf_combining/main.py
+++ elf_combining/main.py
@@ -1,21 +1,18 @@
 import os
 import contextlib
 import mmap
-
+out_name = "a_hello"
+header_name = "header"
+main_name = "test"
 temp_key = b'\x8eNT\xdee\x96\xda\xec'
-path_out = "out/a"
-path_header = "in/header"
-path_main = "in/bubble_sort"
-# path_mixed = "in/elf64header"
 
-compile_header = "nasm -fbin -o in/header src/header.asm"
-compile_main = "nasm -fbin -o in/bubble_sort src/bubble_sort.asm"
-compile_mixed = "nasm -fbin -o in/elf64header src/elf64header.asm"
+compile_header = f"nasm -fbin -o in/{header_name} src/{header_name}.asm"
+compile_main = f"nasm -fbin -o in/{main_name} src/{main_name}.asm"
 
 os.system(compile_header)
-header_size = os.path.getsize(path_header)
+header_size = os.path.getsize(f"in/{header_name}")
 
-with open("src/bubble_sort.asm", 'r+', encoding='utf8') as asm:
+with open(f"src/{main_name}.asm", 'r+', encoding='utf8') as asm:
     asm_txt = asm.read()
     j = 19
     s = asm_txt[j]
@@ -27,33 +24,29 @@ with open("src/bubble_sort.asm", 'r+', encoding='utf8') as asm:
     asm.seek(19)
     print(hex(header_size + 0x00400000))
     asm.write(hex(header_size + 0x00400000)+"\n")
-    # asm.write(asm_txt)
 
 os.system(compile_main)
-# os.system(compile_mixed)
 
-file_size = os.path.getsize(path_main)
-# mixed_size = os.path.getsize(path_mixed)
+file_size = os.path.getsize(f"in/{main_name}")
 
 print(f"header size: {header_size}")
 print(f"file size: {file_size}")
 print(f"header_size + file_size: {header_size + file_size}")
-# print(f"mixed size: {mixed_size}")
 
 i = 0
-with open(path_main, 'rb') as out:
+with open(f"in/{main_name}", 'r+b') as out:
     main = list(out.read())
 # XOR
-# while i < file_size:
-#     main[i] = main[i] ^ temp_key[i % 8]
-#     i += 1
+while i < file_size:
+    main[i] = main[i] ^ temp_key[len(temp_key) - 1 - (i % 8)]
+    i += 1
 
-with open(path_out, "wb+") as out, open(path_header, "rb") as header:
+with open(f"out/{out_name}", "wb+") as out, open(f"in/{header_name}", "rb") as header:
     out.write(header.read())
     out.write(bytearray(main))
 
 a_bytes_little = (header_size + file_size).to_bytes(8, 'little')
-with open(path_out, 'r+b') as out:
+with open(f"out/{out_name}", 'r+b') as out:
     out.seek(0x60)
     out.write(a_bytes_little)
     out.write(a_bytes_little)
